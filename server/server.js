17aa76f95e22e68d19e4af144789dae213a571c3
joshuatjoy <joshua2232004@gmail.com>
2025-07-15 23:52:48 +0530

    stripe added

--- server/server.js
+++ server/server.js
@@ -5,11 +5,21 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import showRouter from './routes/showRoutes.js';
+import bookingRouter from './routes/bookingRoutes.js';
+import adminRouter from './routes/adminRoutes.js';
+import userRouter from './routes/userRoutes.js';
+import { stripeWebhooks } from './controllers/stripeWebhooks.js';
 
 const app = express();
 const port = 3000;
 
 await connectDB()
+
+
+//Stripe Webhooks route
+
+app.use('/api/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
 //Middleware
 
@@ -21,6 +31,11 @@ app.use(clerkMiddleware())
 //API routes
 app.get('/', (req,res) => res.send("Server is Live"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use('/api/show', showRouter)
+app.use('/api/booking', bookingRouter)
+app.use('/api/admin', adminRouter)
+app.use('/api/user', userRouter)
+
 
 
 app.listen(port, () => {
